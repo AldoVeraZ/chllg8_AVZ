6ee8a17888ad18ddc40fd4a896cf7a4e456a4043
Aldo Esteban Vera Zúñiga <aestebanveraz@gmail.com>
2023-12-11 18:22:33 -0300

    bla

--- js/main.js
+++ js/main.js
@@ -40,3 +40,34 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var imagenContainer = document.getElementById("imagen-container");
+  var parrafoContainer = document.getElementById("parrafo-container");
+
+  // Crear un elemento de imagen
+  var imagen = new Image();
+  imagen.src = "./homelander.jpg"; // Cambia esto con la ruta de tu imagen
+  imagenContainer.appendChild(imagen);
+
+  // Crear un elemento de párrafo con un span para el color
+  var parrafo = document.createElement("p");
+  var span = document.createElement("span");
+  span.textContent = "Seven Shredders!";
+  span.style.color = "red"; // Cambia el color según tus preferencias
+  parrafo.appendChild(span);
+  parrafo.innerHTML += ", El único gimnasio donde quedarás como homelander!";
+  parrafoContainer.appendChild(parrafo);
+
+  // Verificar si el enlace al CSS está presente y activo
+  var cssLink = document.querySelector('link[rel="stylesheet"]');
+  if (cssLink && !cssLink.disabled) {
+    // Si el CSS está activo, mostrar la imagen y el párrafo
+    imagenContainer.style.display = "block";
+    parrafoContainer.style.display = "block";
+  } else {
+    // Si el CSS está desactivado, ocultar la imagen y el párrafo
+    imagenContainer.style.display = "none";
+    parrafoContainer.style.display = "none";
+  }
+});
